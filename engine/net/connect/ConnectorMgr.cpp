cd391e46d63bd449975d9b1c53b68aef92f4ffe3
aiyou4love <aiyou4love@outlook.com>
2017-01-15 12:52:59 +0800

    u

--- engine/net/connect/ConnectorMgr.cpp
+++ engine/net/connect/ConnectorMgr.cpp
@@ -49,6 +49,7 @@ namespace cc {
 		SessionPtr session_(new Session(++mSessionId, ioHandle_));
 		session_->setRemove(&connectRemove_);
 		session_->setAppId(nAppId);
+		session_->setAuthority(Eauthority::mSystem);
 		mSessions[nAppId] = session_;
 		return mSessions[nAppId];
 	}
